79e25b0ce75cadae7e9f8b8b68ba533d87c7cd31
Wesley Lin <wesley.lin@kcl.ac.uk>
2022-10-02 20:12:17 +0100

    It works now

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    x = number_of_primes
     list = []
+    no = 0
+    i = 1
+    while not(no == x):
+        factors = 0
+        
+        i = i + 1
+        for j in range(1,i):
+            if(i % j == 0):
+                factors = factors + 1
+            if(factors == 2):
+                list.append(i)
+                no = no + 1
     return list
